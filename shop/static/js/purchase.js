f69ac43637b3f18bf8c49dcb4f788dd34ef7c348
nuriaoyaga <nuria.oyaga@gmail.com>
2017-04-25 19:30:40 +0200

    Modificado carrito

--- shop/static/js/purchase.js
+++ shop/static/js/purchase.js
@@ -1,16 +1,22 @@
-function init(){
+function init(data1,data2,type,price){
   cart =[]
   localStorage.setItem("cart", JSON.stringify(cart));
   cart = JSON.parse(localStorage.getItem('cart'));
   console.log(cart)
+  addProduct(data1,data2,type,price)
 }
 
 function addProduct(data1,data2,type,price){
   if (typeof(Storage) !== "undefined") {
-    cart = JSON.parse(localStorage.getItem('cart'));
-    cart.push([data1,data2,type,price])
-    console.log(cart)
-    localStorage.setItem("cart", JSON.stringify(cart));
+    try {
+      cart = JSON.parse(localStorage.getItem('cart'));
+      cart.push([data1,data2,type,price])
+      console.log(cart)
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } catch (e) {
+      console.log(e);
+      init(data1,data2,type,price)
+    }
   } else {
     console.log("Sorry, your browser does not support Web Storage...");
   }
